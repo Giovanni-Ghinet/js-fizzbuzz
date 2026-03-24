ac22bfc39a15662c297838681867457c59e20501
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-03-24 13:31:36 +0100

    usato i commenti per ragionare sulla struttura

---
+++ script.js
@@ -0,0 +1,9 @@
+// Per ogni numero da 1 a 100
+//   se il numero è divisibile per 3 e per 5:
+//     stampa "FizzBuzz"
+//   altrimenti, se è divisibile solo per 3:
+//     stampa "Fizz"
+//   altrimenti, se è divisibile solo per 5:
+//     stampa "Buzz"
+//   altrimenti:
+//     stampa il numero
