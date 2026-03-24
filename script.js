fbc0723c819de3be21418f5815ccce8420591b93
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-03-24 13:48:40 +0100

    utilizzato ciclo for per far si che stampi ogni numero e inserito un if per capire se il numero è divisibile per 3 o 5 nel caso fosse possibile al posto del numero stampa la stringa nella seconda parte commentata c'è la versione minimal che ti permette di fare tutto il calcolo nel log

--- script.js
+++ script.js
@@ -7,3 +7,25 @@
 //     stampa "Buzz"
 //   altrimenti:
 //     stampa il numero
+
+// in questo modo il codice js è stato diviso tramite if per rendere molto più chiaro i passaggi
+
+for (let i = 1; i <=100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+
+// facendo questa versione possiamo calcolare direttamente se è divisibile per 3 
+// o per 5 così da stampare una stringa solo qunado è divisibile e se sono vere entrambe le stampa insieme
+
+/* for (let i = 1; i <= 100; i++) {
+  console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
+} */
